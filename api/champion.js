2cb8e6802083e05da6dd03e28ac38d309a3521c0
akiakiu <ruzgary799@gmail.com>
2026-02-14 22:57:35 +0300

    Update champion.js

--- api/champion.js
+++ api/champion.js
@@ -1,10 +1,16 @@
+// .env dosyasını okumak için
+require("dotenv").config();
+
+// Key artık env'den geliyor
+const RIOT_API_KEY = process.env.RIOT_API_KEY;
+
 export default async function handler(req, res) {
   try {
     const response = await fetch(
       "https://euw1.api.riotgames.com/lol/platform/v3/champion-rotations",
       {
         headers: {
-       
+          "X-Riot-Token": RIOT_API_KEY
         }
       }
     );
